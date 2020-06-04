4c259b261bd550c2a1cc5d859161017be07e655d
senadakadric <skadric@fordham.edu>
2020-06-04 17:50:27 -0400

    Created routes and views for viewing all artists and individual artist instances

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -11,4 +11,16 @@ class ApplicationController < Sinatra::Base
     erb :welcome
   end
 
+  get "/artists" do
+    @artists = Artist.all
+
+    erb :index
+  end
+
+  get "/artists/:id" do
+    @artist = Artist.find(params[:id])
+
+    erb :show
+  end
+
 end
